7113d4ecaf51b06532b71159a323b29a61e0fd2c
Deepak <57610657+bug-vs-me@users.noreply.github.com>
2023-04-30 22:01:41 +0530

    hackerone asset finder
    Hackerone assets collector tool

---
+++ h1finder.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Set your HackerOne API key
+API_USERNAME="<your hackerone username>"
+API_KEY="< Hackerone api key>"
+
+# Retrieve list of programs
+page=1
+while [ true ]
+do
+  response=$(curl -X GET -u "$API_USERNAME:$API_KEY" -H "Accept: application/json" "https://api.hackerone.com/v1/hackers/programs/?page%5Bnumber%5D=$page&page%5Bsize%5D=100")
+  if [ "$(echo $response | jq '.data | length')" -eq 0 ]
+  then
+    break
+  fi
+  echo "$response" | jq -r '.data[] | select(.attributes.offers_bounties == true) | .attributes.handle' >> programs.txt
+  ((page++))
+done
+
+# Retrieve domains for each program
+sort -u programs.txt -o programs.txt
+while read -r handle
+do
+  response=$(curl -s -X GET -u "$API_USERNAME:$API_KEY" -H 'Accept: application/json' "https://api.hackerone.com/v1/hackers/programs/$handle" | jq -r '.relationships.structured_scopes.data[] | select(.attributes.asset_type == "URL" and .attributes.eligible_for_submission == true) | .attributes.asset_identifier')
+echo $response >> target.txt
+echo $response
+done < programs.txt
+sort -u target.txt -o target.txt
+cat target.txt | tr -s ' ' '\n' | sort -u | sed '/^$/d' > final.txt
+grep '*' final.txt > wild.txt
+grep -v '*' final.txt > nonwild.txt
+sort -u wild.txt -o wild.txt
+echo "wild domain saved to wild.txt"
+sort -u nonwild.txt -o nonwild.txt
+echo " non wild scope domains saved to nonwild.txt"
